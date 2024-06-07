8d53356cec639f67b8ec20e844165d4ac973069d
Eilon Lipton <Eilon@users.noreply.github.com>
2024-06-07 09:42:05 -0700

    Cleanup

--- src/Core/src/Handlers/HybridWebView/HybridWebViewHandler.Standard.cs
+++ src/Core/src/Handlers/HybridWebView/HybridWebViewHandler.Standard.cs
@@ -7,13 +7,5 @@ namespace Microsoft.Maui.Handlers
 		protected override object CreatePlatformView() => throw new NotImplementedException();
 
 		public static void MapSendRawMessage(IHybridWebViewHandler handler, IHybridWebView hybridWebView, object? arg) { }
-
-		//public static void MapUserAgent(IHybridWebViewHandler handler, IHybridWebView webView) { }
-
-		//public static void MapGoBack(IHybridWebViewHandler handler, IHybridWebView webView, object? arg) { }
-		//public static void MapGoForward(IHybridWebViewHandler handler, IHybridWebView webView, object? arg) { }
-		//public static void MapReload(IHybridWebViewHandler handler, IHybridWebView webView, object? arg) { }
-		//public static void MapEval(IHybridWebViewHandler handler, IHybridWebView webView, object? arg) { }
-		//public static void MapEvaluateJavaScriptAsync(IHybridWebViewHandler handler, IHybridWebView webView, object? arg) { }
 	}
 }
